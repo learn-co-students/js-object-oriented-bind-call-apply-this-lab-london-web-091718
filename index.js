3eab6dea08aa7a844c65c221cfc08eaaf3a95a65
Patrick <ctrlaltpat@gmail.com>
2018-11-02 11:27:42 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function setThisWithCall(fn, thisValue, arg) {
 }
 
 function setThisWithApply(fn, thisValue, args) {
-  console.log(args)
+  // console.log(args)
   return fn.apply(thisValue, args);
 }
 
