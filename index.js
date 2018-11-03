d3371c7da304abd899a218d6ea45b1d503e27543
ssong-eu <sangyoon.song@gmail.com>
2018-11-03 23:32:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Your code here
+function justInvoke (fn) {
+  return fn();
+};
+
+function setThisWithCall (fn, thisVal, arg) {
+  return fn.call(thisVal, arg);
+};
+
+function setThisWithApply (fn, thisVal, arg) {
+  return fn.apply(thisVal, arg);
+};
+
+function returnNewFunctionOf (func, thisVal) {
+  return func.bind(thisVal);
+}
